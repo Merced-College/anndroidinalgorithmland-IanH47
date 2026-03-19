321c252e80f4a709c32dbc81cfd9de63e0203bc1
ianha <ianha@LAPTOP-V0358MFR>
2026-03-19 10:31:49 -0700

    3.19-2

--- src/level1/LeaderboardAlgorithms.java
+++ src/level1/LeaderboardAlgorithms.java
@@ -24,7 +24,7 @@ public class LeaderboardAlgorithms {
     	   int j = i;
     	   // Insert numbers[i] into sorted part
     	   // stopping once numbers[i] in correct position
-    	   while (j > 0 && list.get(j).getScore() < list.get(j-1).getScore()) {
+    	   while (j > 0 && list.get(j).getScore() > list.get(j-1).getScore()) {
     	       
     	      // Swap numbers[j] and numbers [j - 1]
     	      ScoreEntry temp = list.get(j);
@@ -70,6 +70,22 @@ public class LeaderboardAlgorithms {
      */
     public static int binarySearchByUsername(ArrayList<ScoreEntry> list, String username) {
         // TODO
+    	
+    	int low = 0;
+        int high = list.size() - 1;
+     
+        while (high >= low) {
+           int mid = (high + low) / 2;
+           if (list.get(mid).getUsername().compareTo(username) < 0) {
+              low = mid + 1;
+           }
+           else if (list.get(mid).getUsername().compareTo(username) > 0) {
+              high = mid - 1;
+           }
+           else {
+              return mid;
+           }
+        }
         return -1;
     }
 }
